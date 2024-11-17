e2a642c2ca45397ffe4f38f91f91778d776ff9cd
MatheusInacio-Git <mateusinacio32@hotmail.com>
2024-11-16 23:53:51 -0300

    atualizacao2

--- src/components/Header1.js
+++ src/components/Header1.js
@@ -5,15 +5,26 @@ import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 export default function Header1() {
   return (
     <motion.header 
-      className="bg-gray-800 text-white p-6 shadow-md flex justify-between items-center"
+      className="absolute top-0 left-0 right-0 bg-cyan-transparent text-white p-6 shadow-md flex justify-between items-center z-10"
       initial={{ y: -50, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.5 }}
-    >
-      <h1 className="text-2xl font-bold">Matheus Nunes Inácio</h1>
+    >  
+      <div className="flex items-center space-x-4 pl-12 ml-3">
+        <img 
+          src="/assets/header.png" 
+          alt="Matheus Nunes Inácio"
+          className="logo rounded-full" 
+        />
+        <div>
+          <h1 className="text-3xl font-bold">Matheus Nunes Inácio</h1>
+          <p className="text-sm text-teal-400">Front-end Developer</p>
+        </div>
+      </div>
+      
       <nav>
         <ul className="flex space-x-6">
-          <li><a href="#home" className="hover:text-teal-400">Inicio</a></li>
+          <li><a href="#home" className="hover:text-teal-400">Início</a></li>
           <li><a href="#sobre" className="hover:text-teal-400">Sobre Mim</a></li>
           <li><a href="#habilidades" className="hover:text-teal-400">Habilidades</a></li>
           <li><a href="#experience" className="hover:text-teal-400">Experiência</a></li>
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,16 +4,26 @@ import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Home() {
   return (
-    <section id="home" className="bg-transparent text-white min-h-screen flex flex-col justify-center items-center">
+    <section 
+      id="home" 
+      className="bg-transparent text-white min-h-[120vh] flex flex-col justify-center items-start bg-cover bg-center pt-20"
+      style={{
+        backgroundImage: "url('/assets/image3.png')",
+        backgroundSize: 'cover',  // Aplica o efeito de zoom na imagem
+        backgroundPosition: 'center',  // Centraliza a imagem
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
       <motion.div
         className="text-center"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1.5 }}
       >
-        <h1 className="text-4xl md:text-6xl font-bold mb-4">Matheus Nunes Inácio</h1>
+        <h1 className="text-4xl md:text-6xl font-bold mb-4 pl-20">Hey, I'm Matheus 👋</h1>
+        <h1 className="text-4xl md:text-5xl mb-4 pl-20 ml-10">I'm a Front-end Developer.</h1>
         <p className="text-xl">Programador Full Stack</p>
-        <div className="flex space-x-4 mt-6">
+        <div className="flex space-x-4 mt-6 ml-20 pl-2">
           <a href="https://linkedin.com/in/matheusnunesinacio" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faLinkedin} className="text-blue-500 text-2xl hover:scale-110 transition-transform" />
           </a>
