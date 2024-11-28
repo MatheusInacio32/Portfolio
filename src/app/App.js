8c7944b4de139d4e3ef0302ab80389d01c867671
MatheusInacio-Git <mateusinacio32@hotmail.com>
2024-11-28 15:26:12 -0300

    update

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,32 @@
 // src/app/App.js
 import '../styles/tailwind.css';
 import React from 'react';
+import Header1 from '../components/Header1';
+import Home from '../components/Home';
+import Sobre from '../components/Sobre';
+import Experience from '../components/Experience';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
+import Habilidades from '../components/Habilidades';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import Portifolio from '../pages/Portifolio';
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 library.add(fas, fab);
 
 const App = () => {
   return (
-    <Router>
-    <Routes>
-      <Route path="/" element={<Portifolio />} />
-    </Routes>
-  </Router>
+      <div className='bg-gradient-to-b from-black to-gray-900'>
+        <Header1 />
+        <main>
+          <div><Home/></div>
+          <div><Sobre/></div>
+          <div><Habilidades/></div>
+          <div><Experience/></div>
+          <div><Projects/></div>
+          <div><Contact/></div>
+        </main>
+      </div>
   );
 }
 
